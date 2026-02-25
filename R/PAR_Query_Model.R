6a96534f0c95e5c391ebe462af56ed686905801c
David Rattray <david.rattray@gov.bc.ca>
2026-02-25 11:12:12 -0800

    Updated PAR with keyring
    Also added in commented record of the server logging functions.
    Switched utility to central one for easier maintenance.

--- R/PAR_Query_Model.R
+++ R/PAR_Query_Model.R
@@ -1,3 +1,7 @@
+# For server logging
+# Begin timer
+# task_start <- Sys.time()
+
 # Load necessary packages
 library(base64enc, quietly = TRUE, warn.conflicts = FALSE)
 library(dplyr, quietly = TRUE, warn.conflicts = FALSE)
@@ -10,19 +14,24 @@ library(tidyr, quietly = TRUE, warn.conflicts = FALSE)
 
 # Server Run
 # source("E:/Projects/citz-rebs-jira-api/utilities.R")
+# source("E:/Projects/citz-rpd-utilities/event_logger.R")
+# api_id <- "Jira" # for event_logger
+# project_id <- "PAR" # for event_logger
+# email <- "rpd.spbooking@gov.bc.ca"
+# api_key <- keyring::key_get(
+#   service = "JIRA_API",
+#   username = email,
+#   keyring = NULL
+# )
 
 # Local Run
-source(here::here("R/utilities.R"))
-
-# Update email/api_key as needed
-# Server Run
-# email <- read.csv("E:/Projects/credentials/service_email_address.csv") |> pull()
-# api_key <- read.csv("E:/Projects/credentials/jira_api_service_token.csv") |>
-#   pull()
-
-# Local Run
-email <- read.csv("C:/Projects/credentials/email_address.csv") |> pull()
-api_key <- read.csv("C:/Projects/credentials/jira_api_token.csv") |> pull()
+source("C:/Projects/citz-rpd-utilities/safe_hoist.R")
+email <- "david.rattray@gov.bc.ca"
+api_key <- keyring::key_get(
+  service = "JIRA_API",
+  username = email,
+  keyring = NULL
+)
 
 # Encode token
 token <- base64encode(charToRaw(paste0(email, ":", api_key)))
@@ -32,21 +41,20 @@ token_string <- paste("Basic", token)
 query_url = "https://citz-rpd.atlassian.net/rest/api/3/search/jql"
 # https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-issue-search/#api-rest-api-3-search-jql-get
 # https://developer.atlassian.com/changelog/#CHANGE-2046
-project_id = "PAR"
+dashboard_id = "PAR"
 expand_opts = c("names", "fields")
 max_results = 100
 nextPageToken = NULL
-total_results = 1
 progress = 0
 round = 1
 
 # Issues Loop ####
-while (total_results > progress) {
+while (progress < 2) {
   req <- request(query_url) |>
     req_headers(Authorization = token_string) |>
     # configure project, max_results, and start_at
     req_url_query(
-      jql = I(paste0("project=", project_id)), # I wrapper skips auto-formatting of the extra "=" sign
+      jql = I(paste0("project=", dashboard_id)), # I wrapper skips auto-formatting of the extra "=" sign
       expand = expand_opts,
       maxResults = max_results,
       fields = "*all",
@@ -54,7 +62,52 @@ while (total_results > progress) {
       nextPageToken = nextPageToken,
       .multi = "comma" # control how vectors are appended, for expand_opts
     ) |>
+    # Server logging and proxy steps
     # req_proxy("142.34.229.249", 8080) |> # Use Server Proxy to connect
+    # req_error(
+    #   is_error = function(resp) {
+    #     lr <- resp_header(resp, "x-seraph-loginreason")
+    #     bad_auth <- !is.null(lr) &&
+    #       grepl("AUTHENTICATED_FAILED|AUTHENTICATION_DENIED", lr)
+    #     empty_ok <- FALSE # we only care about bad_auth here
+    #     bad_auth || empty_ok
+    #   },
+
+    #   body = function(resp) {
+    #     paste0(
+    #       "Auth Failure for ",
+    #       api_id,
+    #       " reason: ",
+    #       resp_header(resp, "x-seraph-loginreason") %||% "UNKNOWN",
+    #       " traceid: ",
+    #       resp_header(resp, "atl-traceid") %||% "NA",
+    #       " url: ",
+    #       resp_url(resp)
+    #     )
+    #   }
+    # )
+
+    # Perform request with error handling and structured logging
+    # resp <- tryCatch(
+    #   req_perform(req) |> resp_body_json(),
+    #   error = function(e) {
+    #     # Compose a one-line description with context
+    #     desc <- if (!is.null(e$body) && is.character(e$body)) {
+    #       e$body
+    #     } else {
+    #       e$message
+    #     }
+
+    #     # Log the error to daily CSV
+    #     event_logger(
+    #       api = api_id,
+    #       subset = project_id, # PAR / SBP / RBAS
+    #       event_type = "error",
+    #       description = desc
+    #     )
+    #     stop(e) # rethrow so task scheduler flags a failure (current monitoring is by Nagios)
+    #   }
+    # )
     req_perform()
 
   resp <- req |> resp_body_json()
@@ -171,7 +224,11 @@ Issues <- Issues %>%
   replace_na(replace = list(TimeToCompletion = NaN))
 
 # Server Save
+# task_time <- as.numeric(difftime(Sys.time(), task_start, units = "secs"))
+# desc <- paste0("Run Completed, ", nrow(Issues), " tickets processed. Runtime: ", task_time)
+# event_logger(api_id, project_id, "success", desc)
+
 # write.csv(Issues, "E:/Projects/PBI-Gateway/PAR_Issues.csv", row.names = FALSE)
 
 # Local Save
-write.csv(Issues, here::here("PAR_output.csv"))
+# write.csv(Issues, here::here("PAR_output.csv"))
